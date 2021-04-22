94d149a76ee942863ee365c8cba9e47b3f857bad
Dave Spina <dspina79@gmail.com>
2021-04-21 21:10:49 -0400

    Added startup method

--- BeaconDetector/BeaconDetector/BeaconDetector.swift
+++ BeaconDetector/BeaconDetector/BeaconDetector.swift
@@ -30,4 +30,13 @@ class BeaconDetector: NSObject, ObservableObject, CLLocationManagerDelegate {
             }
         }
     }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let constraint = CLBeaconIdentityConstraint(uuid: uuid, major: 123, minor: 456)
+        
+        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: "MyBeacon")
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: constraint)
+    }
 }
